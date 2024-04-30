f318a71a37bc0ab7bc51e3bd1a0d722f55dc6d47
Margot Marchais <margot.marchais.maurice@gmail.com>
2024-04-30 16:43:25 +0000

    Deleted all sub-datasets related to Jeff de Bruges, added consolidates datasets related to Jeff de Bruges, Lindt, Alain Ducasse, Maison du Chocolat

--- models/1_Raw_data/jeff_de_bruges_stg.sql
+++ models/1_Raw_data/jeff_de_bruges_stg.sql
@@ -10,4 +10,4 @@ SELECT DISTINCT
     product_url, 
     price AS product_price,
     weight_grams AS product_weight_g
-FROM `dbt-chocolate-project.dbt_chocolate_setup.jdb_glaces` 
+FROM `dbt-chocolate-project.dbt_chocolate_setup.jeff_de_bruges` 
--- models/3_Production/Jeff_de_Bruges.sql
+++ models/3_Production/Jeff_de_Bruges.sql
@@ -1,15 +1,2 @@
 -- Objective : Create an exhaustive dataset for all Jeff de Bruges products
 
-SELECT *
-FROM {{ref("jdb_chocolates")}}
-
-UNION DISTINCT
-
-SELECT *
-FROM {{ref("jdb_dragees")}}
-
-UNION DISTINCT
-
-SELECT *
-FROM {{ref("jdb_glaces")}}
-
