d18c740d62ac0a957f5508c18fdf6511fb98f4a8
Margot Marchais <margot.marchais.maurice@gmail.com>
2024-04-30 18:57:16 +0000

    Created the whole Chocolate dataset

--- models/1_Raw_data/maison_du_chocolat_stg.sql
+++ models/1_Raw_data/maison_du_chocolat_stg.sql
@@ -4,14 +4,22 @@
   )
 }}
 
-SELECT DISTINCT
-    date_photo,
-    product_name, 
-    image_url, 
-    product_url, 
-    price AS product_price,
-    product_weight AS product_weight_g,
-    quantity,
-    rating,
-    product_description
-FROM `dbt-chocolate-project.dbt_chocolate_setup.maison_du_chocolat` 
+WITH base AS (
+    SELECT DISTINCT
+        timestamp_photo,
+        date_photo,
+        product_name, 
+        image_url, 
+        product_url, 
+        price AS product_price,
+        product_weight AS product_weight_g,
+        quantity,
+        rating,
+        product_description,
+        DENSE_RANK() OVER (PARTITION BY product_name ORDER BY timestamp_photo DESC) AS RANK_SCRAP
+    FROM `dbt-chocolate-project.dbt_chocolate_setup.maison_du_chocolat` 
+)
+
+SELECT *
+FROM base
+WHERE RANK_SCRAP = 1
