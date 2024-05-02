690ae492d9e2152d142dfd5d0ddd563999da9183
Margot Marchais <margot.marchais.maurice@gmail.com>
2024-05-02 13:35:12 +0000

    Updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Chocolate
+# Market study | Chocolates French Market
+
+
+*Executive summary*: My goal for this project was to collect data from various chocolate 
+makers and distributors in France, and eventually build a comprehensive dataset about the chocolates market in France.
+
+*Methodology:*
+- I scraped product data for several French e-commerce websites (chocolates section), using Python scrapy and requests libraries
+- I created a new, empty project in GCP BigQuery
+- I then build a script in Python to: 
+    - do some minor transformations to the .csv files resulting from web scraping (data cleaning)
+    - automatically load the resulting dataframes in BigQuery using the bigquery.client 
+- I created a dbt Cloud project that is connected to my BigQuery project
+- In this dbt project, I created 3 sections:
+    - Bronze (staging): raw data, with few modifications.
+    - Silver (transformations): data with some transformations (new columns, filters, etc)
+    - Gold (final): final datasets that will be used for analysis.
+- Thanks to dbt, I could 'export' the SQL views and tables to GCP BigQuery. I also managed to create dbt QoD tests and documentation For the intermediate and final outputs.
+All dbt modifications were saved thanks to dbt-Github
+- Finally, I could plug Power BI to my Gold final datasets to build a visual overview of the market
+
+*Final output*
+- A comprehensive dataset about the French chocolates online market
+- A visual Power BI dashboard
+
+*Technical learnings:* I did this project to help me acquire new skills such as:
+build a datawharehouse in GCP Bigquery, feed this DWH with scraped data automatically thanks to a Python script, 
+learn how to use dbt (data transformations, tests and docs generation),... It also made me refresh my web scraping skills (scrapy and requests libraries).
+
---
+++ models/3_Production/brand_statistics.sql
@@ -0,0 +1,98 @@
+-- Objective : Create basic summary statistics for each brand available in the Chocolates dataset
+
+-- Compute the price/kg
+WITH base AS (
+    SELECT 
+        t.*,
+        CASE 
+            WHEN product_price IS NOT NULL AND product_weight_g IS NOT NULL
+                THEN  ROUND(product_price / product_weight_g * 1000,2)
+            ELSE NULL
+        END AS price_per_kilo
+    FROM {{ref('Chocolates')}} AS t
+),
+
+-- Get the dominant subcategory for each brand (i.e. the subcategory that offers the most diverse products, by brand)
+dom_subcateg AS (
+    SELECT 
+        brand, 
+        subcategory AS dom_subcategory,
+        nb_products_subcateg AS dom_subcateg_nb_products
+    FROM (
+        SELECT 
+            brand, 
+            subcategory, 
+            nb_products_subcateg,
+            DENSE_RANK() OVER (PARTITION BY brand ORDER BY nb_products_subcateg DESC, subcategory DESC) AS ranking_subcateg
+        FROM (
+            SELECT 
+                brand, 
+                subcategory,
+                COUNT(*) AS nb_products_subcateg
+            FROM {{ref('Chocolates')}}
+            GROUP BY 1,2
+        )
+    )
+    WHERE ranking_subcateg = 1
+),
+
+
+-- Get the most and least expensive product for each brand
+product_pricing AS (
+    SELECT DISTINCT
+        brand, 
+        -- product_name,
+        -- product_price,
+        brand_min_price AS brand_min_price_product,
+        brand_max_price AS brand_max_price_product,
+        MAX(CASE WHEN product_price = brand_min_price THEN product_name ELSE NULL END) OVER (PARTITION BY brand) AS product_min_price,
+        MAX(CASE WHEN product_price = brand_max_price THEN product_name ELSE NULL END) OVER (PARTITION BY brand) AS product_max_price,
+    FROM (
+        SELECT 
+            brand, 
+            product_name,
+            product_price,
+            MAX(product_price) OVER (PARTITION BY brand) AS brand_max_price,
+            MIN(product_price)  OVER (PARTITION BY brand) AS brand_min_price
+        FROM `dbt-chocolate-project.dbt_mmarchaismaurice.Chocolates` 
+    )
+    WHERE product_price = brand_min_price
+        OR product_price = brand_max_price
+),
+
+-- Aggregate statistics per brand
+agg AS (
+    SELECT 
+        base.brand,
+        COUNT(DISTINCT base.company) AS nb_distrib_channels,
+        COUNT(DISTINCT base.product_name) AS nb_products,
+        COUNT(DISTINCT base.subcategory) AS nb_subcateg,
+        COUNT(DISTINCT base.subcategory2) AS nb_subcateg2,
+        ROUND(AVG(base.price_per_kilo),2) AS avg_price_per_kg,
+        MIN(base.price_per_kilo) AS min_price_per_kg,
+        MAX(base.price_per_kilo) AS max_price_per_kg
+
+    FROM base
+    GROUP BY 1
+)
+
+
+-- All together
+SELECT 
+    agg.*
+    , dom_subcateg.dom_subcategory
+    , dom_subcateg.dom_subcateg_nb_products
+    , brand_min_price_product
+    , brand_max_price_product
+    , product_min_price
+    , product_max_price
+
+FROM agg 
+
+LEFT JOIN dom_subcateg
+    ON agg.brand = dom_subcateg.brand
+
+LEFT JOIN product_pricing
+    ON agg.brand = product_pricing.brand
+
+ORDER BY brand 
