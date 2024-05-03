7735d3edaaa2e90ef8b60448011a5d4097251dff
Margot Marchais <margot.marchais.maurice@gmail.com>
2024-05-03 12:48:51 +0000

    Refined product categories and removed accents from brands and product names

--- models/1_Raw_data/franprix_stg.sql
+++ models/1_Raw_data/franprix_stg.sql
@@ -18,6 +18,15 @@ WITH base AS (
     FROM `dbt-chocolate-project.dbt_chocolate_setup.franprix` 
 )
 
-SELECT *
+SELECT 
+    timestamp_photo,
+    date_photo,
+    brand,
+    REGEXP_REPLACE(NORMALIZE(product_name, NFD), r"\pM", '') AS product_name,
+    image_url, 
+    product_price,
+    price_per_kilo,
+    product_weight_g,
+    RANK_SCRAP
 FROM base
 WHERE RANK_SCRAP = 1
--- models/2_Consolidated_raw_data/alain_ducasse.sql
+++ models/2_Consolidated_raw_data/alain_ducasse.sql
@@ -15,6 +15,8 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%GANACHES%' THEN 'Boites'
             WHEN UPPER(product_name) LIKE '%POT%' THEN 'Boites'
             WHEN UPPER(product_name) LIKE '%CORNET%' THEN 'Boites'
+            WHEN UPPER(product_name) LIKE '%BARQUETTE%' THEN 'Boites'
+            WHEN UPPER(product_name) LIKE '%PANIERE%' THEN 'Boites'
             WHEN UPPER(product_name) LIKE '%TABLETTE%' THEN 'Tablettes'
             WHEN UPPER(product_name) LIKE '%BARRE%' THEN 'Tablettes'
             WHEN UPPER(product_name) LIKE '%ECORCE%' THEN 'Tablettes'
@@ -24,6 +26,7 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%SUCETTE%' THEN 'Sucreries'
             WHEN UPPER(product_name) LIKE '%MARSHMALLOW%' THEN 'Sucreries'
             WHEN UPPER(product_name) LIKE '%OURS%GUIMAUVE%' THEN 'Sucreries'
+            WHEN UPPER(product_name) LIKE '%BILLE%' THEN 'Sucreries'
             WHEN UPPER(product_name) LIKE '%CALISSON%' THEN 'Biscuits'
             WHEN UPPER(product_name) LIKE '%MENDIANT%' THEN 'Biscuits'
             WHEN UPPER(product_name) LIKE '%BISCUIT%' THEN 'Biscuits'
@@ -34,6 +37,14 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%PISTACHE%' THEN 'Fruits secs'
             WHEN UPPER(product_name) LIKE '%MOULAGE%' THEN 'Moulage'
             WHEN UPPER(product_name) LIKE '%OEUF%' THEN 'Moulage'
+            WHEN UPPER(product_name) LIKE '%PEPITE%' THEN 'Autres'
+            WHEN UPPER(product_name) LIKE '%NOUGAT%' THEN 'Autres'
+            WHEN UPPER(product_name) LIKE '%TRUFFES%' THEN 'Autres'
+            WHEN UPPER(product_name) LIKE '%CALENDRIER%' THEN 'Collection spéciale'
+            WHEN UPPER(product_name) LIKE '%NOEL%' THEN 'Collection spéciale'
+            WHEN UPPER(product_name) LIKE '%VALENTIN%' THEN 'Collection spéciale'
+            WHEN UPPER(product_name) LIKE '%PAQUES%' THEN 'Collection spéciale'
+            WHEN UPPER(product_name) LIKE '%MUGUET%' THEN 'Collection spéciale'
             ELSE 'Autres'
         END AS subcategory,
         CASE 
@@ -43,6 +54,8 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%BALLOTIN%' THEN 'Ballotin'
             WHEN UPPER(product_name) LIKE '%GANACHES%' THEN 'Ganache'
             WHEN UPPER(product_name) LIKE '%POT%' THEN 'Pot'
+            WHEN UPPER(product_name) LIKE '%BARQUETTE%' THEN 'Barquette'
+            WHEN UPPER(product_name) LIKE '%PANIERE%' THEN 'Panière'
             WHEN UPPER(product_name) LIKE '%CORNET%' THEN 'Cornet'
             WHEN UPPER(product_name) LIKE '%TABLETTE%' THEN 'Tablette'
             WHEN UPPER(product_name) LIKE '%BARRE%' THEN 'Barre'
@@ -51,6 +64,7 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%SORBET%' THEN 'Sorbet'
             WHEN UPPER(product_name) LIKE '%CR_ME%GLAC_E%' THEN 'Crème glacée'
             WHEN UPPER(product_name) LIKE '%SUCETTE%' THEN 'Sucette'
+            WHEN UPPER(product_name) LIKE '%BILLE%' THEN 'Billes'
             WHEN UPPER(product_name) LIKE '%MARSHMALLOW%' THEN 'Marshmallows'
             WHEN UPPER(product_name) LIKE '%OURS%GUIMAUVE%' THEN 'Ours guimauve'
             WHEN UPPER(product_name) LIKE '%CALISSON%' THEN 'Calisson'
@@ -63,6 +77,14 @@ WITH base AS (
             WHEN UPPER(product_name) LIKE '%PISTACHE%' THEN 'Pistaches'
             WHEN UPPER(product_name) LIKE '%MOULAGE%' THEN 'Moulage'
             WHEN UPPER(product_name) LIKE '%OEUF%' THEN 'Oeuf'
+            WHEN UPPER(product_name) LIKE '%PEPITE%' THEN 'Pépites'
+            WHEN UPPER(product_name) LIKE '%NOUGAT%' THEN 'Nougat'
+            WHEN UPPER(product_name) LIKE '%TRUFFES%' THEN 'Truffes'
+            WHEN UPPER(product_name) LIKE '%CALENDRIER%' THEN 'Calendrier avent'
+            WHEN UPPER(product_name) LIKE '%NOEL%' THEN 'Noël'
+            WHEN UPPER(product_name) LIKE '%VALENTIN%' THEN 'Saint-Valentin'
+            WHEN UPPER(product_name) LIKE '%PAQUES%' THEN 'Pâques'
+            WHEN UPPER(product_name) LIKE '%MUGUET%' THEN 'Muguet'
             ELSE 'Autres'
         END AS subcategory2,
         base.*
@@ -74,7 +96,7 @@ SELECT
     company,
     brand,
     category,
-    CASE
+    CASE 
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%BO_TE%' THEN 'Boites'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%COFFRET%' THEN 'Boites'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%SACHET%' THEN 'Boites'
@@ -82,6 +104,8 @@ SELECT
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%GANACHES%' THEN 'Boites'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%POT%' THEN 'Boites'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%CORNET%' THEN 'Boites'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%BARQUETTE%' THEN 'Boites'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%PANIERE%' THEN 'Boites'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%TABLETTE%' THEN 'Tablettes'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%BARRE%' THEN 'Tablettes'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%ECORCE%' THEN 'Tablettes'
@@ -91,6 +115,7 @@ SELECT
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%SUCETTE%' THEN 'Sucreries'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%MARSHMALLOW%' THEN 'Sucreries'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%OURS%GUIMAUVE%' THEN 'Sucreries'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%BILLE%' THEN 'Sucreries'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%CALISSON%' THEN 'Biscuits'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%MENDIANT%' THEN 'Biscuits'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%BISCUIT%' THEN 'Biscuits'
@@ -101,6 +126,14 @@ SELECT
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%PISTACHE%' THEN 'Fruits secs'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%MOULAGE%' THEN 'Moulage'
         WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%OEUF%' THEN 'Moulage'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%PEPITE%' THEN 'Autres'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%NOUGAT%' THEN 'Autres'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%TRUFFES%' THEN 'Autres'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%CALENDRIER%' THEN 'Collection spéciale'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%NOEL%' THEN 'Collection spéciale'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%VALENTIN%' THEN 'Collection spéciale'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%PAQUES%' THEN 'Collection spéciale'
+        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%MUGUET%' THEN 'Collection spéciale'
         ELSE subcategory
     END AS subcategory,
     CASE 
@@ -128,8 +161,16 @@ SELECT
         WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%AMANDE%' THEN 'Amandes'
         WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%NOISETTE%' THEN 'Noisettes'
         WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%PISTACHE%' THEN 'Pistaches'
-        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%MOULAGE%' THEN 'Moulage'
-        WHEN subcategory = 'Autres' AND UPPER(product_url) LIKE '%OEUF%' THEN 'Oeufs'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%MOULAGE%' THEN 'Moulage'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%OEUF%' THEN 'Oeufs'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%PEPITE%' THEN 'Pépites'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%NOUGAT%' THEN 'Nougat'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%TRUFFES%' THEN 'Truffes'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%CALENDRIER%' THEN 'Calendrier avent'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%NOEL%' THEN 'Noël'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%VALENTIN%' THEN 'Saint-Valentin'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%PAQUES%' THEN 'Pâques'
+        WHEN subcategory2 = 'Autres' AND UPPER(product_url) LIKE '%MUGUET%' THEN 'Muguet'
         ELSE subcategory2
     END AS subcategory2,
     product_name, 
